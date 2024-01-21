ca320d9fff248b1cb42eddcb1ae993b505e80cef
Middyah <155668752+Middyah@users.noreply.github.com>
2024-01-21 20:14:48 +0530

    Update Navbar2.js

--- src/component/Navbar2.js
+++ src/component/Navbar2.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import '../component/css/Navbar.css'; // Import your CSS file for Navbar styling
-import LocationDropdown from './LocationDropdown'; // Import your LocationComponent
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../component/css/Navbar2.css';
+import LocationDropdown from './LocationDropdown';
 import PopupPage from './PopupPage';
 
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearch = (e) => {
+    if (e.key === 'Enter') {
+      navigate(`/mainarea?post_title=${searchQuery}`);
+    }
+  };
 
   return (
     <nav className="navbar">
       <div className="navbar-content">
-        <LocationDropdown />
-        
-         <PopupPage/>
-        
+        <div className="left-component">
+          <LocationDropdown />
+        </div>
+
+        <div className="center-component">
+          <input
+            type="text"
+            placeholder="Search here"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyDown={handleSearch}
+          />
+        </div>
+
+        <div className="right-component">
+          <PopupPage/>
+        </div>
       </div>
     </nav>
   );
