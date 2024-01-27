61fc1da8609c0f6932ef215995049904f26a2aa0
Middyah <155668752+Middyah@users.noreply.github.com>
2024-01-27 08:00:06 +0530

    Update ImageGalleryPage.js

--- src/component/ImageGalleryPage.js
+++ src/component/ImageGalleryPage.js
@@ -4,14 +4,12 @@ import Card from 'react-bootstrap/Card';
 import apiUrl from '../ApiAxios';
 import InfiniteScroll from "react-infinite-scroll-component";
 import Imagebutton from './Imagebutton';
-import Navbar2 from  './Navbar2'
-
-
+import Navbar2 from './Navbar2'
 const ImageGalleryPage = () => {
   const searchParams = new URLSearchParams(document.location.search)
-let category=searchParams.get('category')
-let post_title=searchParams.get('post_title')
-console.log(post_title,category,"jfghfghg");
+  let category = searchParams.get('category')
+  let post_title = searchParams.get('post_title')
+  console.log(post_title, category, "jfghfghg");
   const [page, setPage] = useState(1);
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState([])
@@ -24,11 +22,11 @@ console.log(post_title,category,"jfghfghg");
     setPage((prevPage) => prevPage + 1);
 
   };
-let reff= localStorage.getItem("reff")
-let location=localStorage.getItem("location")
+  let reff = localStorage.getItem("reff")
+  let location = localStorage.getItem("location")
   const fetchImages = async () => {
     try {
-      let res = await apiUrl.get(`/api/userpost/getallpost?page=${page}&limit=7&category=${category===null?"":category}&post_title=${post_title===null?"":post_title}&location=${location!==null?location:""}`)
+      let res = await apiUrl.get(`/api/userpost/getallpost?page=${page}&limit=7&category=${category === null ? "" : category}&post_title=${post_title === null ? "" : post_title}&location=${location !== null ? location : ""}`)
       setImages((prevData) => [...images, ...res.data.posts])
       setIsLoading(res.data.posts)
     } catch (err) {
@@ -39,20 +37,25 @@ let location=localStorage.getItem("location")
 
   useEffect(() => {
     fetchImages();
-  }, [page,reff]);
+  }, [page, reff]);
 
   const [imageLoaded, setImageLoaded] = useState(false);
   const handleImageLoad = () => {
     setImageLoaded(true);
   };
-  console.log(images, page, "hhh",imageLoaded);
-  
+
+  const handleOpen = (link) => {
+    console.log(link, "link");
+    window.location.assign(link)
+  }
+  console.log(images, page, "hhh", imageLoaded);
   return (
-  <div><Navbar2/>
+   
     <div style={{ textAlign: "center" }}>
-      <h1> </h1>
-      
-    
+       <Navbar2/>
+      <h1>Post Page </h1>
+
+
       <div style={{ textAlign: "center", display: "inline-block" }}>
         <InfiniteScroll
           dataLength={images.length}
@@ -60,7 +63,7 @@ let location=localStorage.getItem("location")
           hasMore={true}
           loader={<>
             {isLoading?.length !== 0 && (
-             <div class="spinner-grow text-danger" role="status">
+              <div class="spinner-grow text-danger" role="status">
                 <span class="sr-only"></span>
               </div>
             )}
@@ -68,25 +71,38 @@ let location=localStorage.getItem("location")
           </>}
 
         >
-          {images.length===0&&<div>No Data Found</div>}
+          {images.length === 0 && <div>No Data Found</div>}
           {images.map((item) => (
-            <Card style={{ width: '18rem', marginBottom: "12px" ,marginBottom:"41px"}}>
-            
-              {!imageLoaded &&(
-               
-                <Card.Img variant="top" src="holder.js/100px180"  style={{height:"199px",backgroundColor:"gray"}}/>
-              
+            <Card style={{ width: '18rem', marginBottom: "12px", marginBottom: "41px" }}>
+              <Card.Text>
+                <div className='headtop' style={{ display: "flex" }}>
+                  <h5 class="card-title" style={{ width: "26%" }}>{item.post_title}</h5>
+                  {item.Link && (
+                    <a
+                      style={{ width: "75%", marginLeft: "71px" }}
+                      href={item.Link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Click here
+                    </a>
+                    // <a style={{width:"75%",marginLeft:"71px"}} href="www.google.com" target='_blank'>Open</a>
+
+                  )}
+                </div>
+              </Card.Text>
+              {!imageLoaded && (
+
+                <Card.Img variant="top" src="holder.js/100px180" style={{ height: "199px", backgroundColor: "gray" }} />
+
               )}
-                <Card.Img variant="top" src={`${process.env.REACT_APP_API_URL}/api/userpost/images/${item._id}`} style={{ backgroundColor: imageLoaded ? "gray" : "red" }}
-                onLoad={handleImageLoad}  className='userimg'/>
-              
+              <Card.Img variant="top" src={`${process.env.REACT_APP_API_URL}/api/userpost/images/${item._id}`} style={{ backgroundColor: imageLoaded ? "gray" : "red" }}
+                onLoad={handleImageLoad} className='userimg' />
+
               <Card.Body>
                 {/* <Card.Title>Card Title</Card.Title> */}
-                <Card.Text>
-                <h5 class="card-title">{item.post_title}</h5>
-                  
-                </Card.Text>
-                <Imagebutton id={item._id} Contact={item.Contactnumber}/>
+
+                <Imagebutton id={item._id} Contact={item.Contactnumber} />
                 {/* <Button variant="primary">Go somewhere</Button> */}
               </Card.Body>
             </Card>
@@ -97,7 +113,6 @@ let location=localStorage.getItem("location")
 
       </div>
     </div>
-  </div>
   );
 };
 
