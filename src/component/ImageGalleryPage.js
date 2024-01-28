63185c13cc4afe58825f2fcf12e8b77d82cf362a
Middyah <155668752+Middyah@users.noreply.github.com>
2024-01-28 09:50:18 +0530

    Update ImageGalleryPage.js

--- src/component/ImageGalleryPage.js
+++ src/component/ImageGalleryPage.js
@@ -1,59 +1,4 @@
-// ImageGalleryPage.js
-import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import apiUrl from '../ApiAxios';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import Imagebutton from './Imagebutton';
-import NavbarPost from './NavbarPost';
-import CustomNavbar from './CustomNavbar';
-
-const ImageGalleryPage = () => {
-  // ... (rest of the code remains unchanged)
-  const searchParams = new URLSearchParams(document.location.search)
-  let category = searchParams.get('category')
-  let post_title = searchParams.get('post_title')
-  console.log(post_title, category, "jfghfghg");
-  const [page, setPage] = useState(1);
-  const [images, setImages] = useState([]);
-  const [isLoading, setIsLoading] = useState([])
-  const handlePagination = () => {
-    if (images && images.length > 0) {
-      setPage((prevPage) => prevPage + 1);
-    }
-  };
-  const loadMore = () => {
-    setPage((prevPage) => prevPage + 1);
-
-  };
-  let reff = localStorage.getItem("reff")
-  let location = localStorage.getItem("location")
-  const fetchImages = async () => {
-    try {
-      let res = await apiUrl.get(`/api/userpost/getallpost?page=${page}&limit=7&category=${category === null ? "" : category}&post_title=${post_title === null ? "" : post_title}&location=${location !== null ? location : ""}`)
-      setImages((prevData) => [...images, ...res.data.posts])
-      setIsLoading(res.data.posts)
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-
-  useEffect(() => {
-    fetchImages();
-  }, [page, reff]);
-
-  const [imageLoaded, setImageLoaded] = useState(false);
-  const handleImageLoad = () => {
-    setImageLoaded(true);
-  };
-
-  const handleOpen = (link) => {
-    console.log(link, "link");
-    window.location.assign(link)
-  }
-  console.log(images, page, "hhh", imageLoaded);
-  return (
+return (
     <div style={{ textAlign: 'center' }}>
       {/* <NavbarPost/> */}
       <CustomNavbar />
@@ -75,11 +20,11 @@ const ImageGalleryPage = () => {
         >
           {images.length === 0 && <div>No Data Found</div>}
           {images.map((item) => (
-            <Card style={{ width: '18rem', marginBottom: '12px', marginBottom: '41px' }}>
+            <Card style={{ width: '27.15vw', marginBottom: '12px', marginBottom: '41px', padding: '1vw' }}>
               {/* ... (rest of the card code remains unchanged) */}
               <Card.Text>
                 <div className='headtop' style={{ display: "flex" }}>
-                  <h5 class="card-title" style={{ width: "26%" }}>{item.post_title}</h5>
+                  <h5 class="card-title" style={{ width: "50%", textAlign: 'left' }}>{item.post_title}</h5>
                   {item.Link && (
                     <a
                       style={{ width: "75%", marginLeft: "71px" }}
@@ -96,17 +41,20 @@ const ImageGalleryPage = () => {
               </Card.Text>
               {!imageLoaded && (
 
-                <Card.Img variant="top" src="holder.js/100px180" style={{ height: "199px", backgroundColor: "gray" }} />
+                <Card.Img variant="top" src="holder.js/100px180" style={{ height: "25vw", width: '40vw', backgroundColor: "gray" }} />
 
               )}
-              <Card.Img variant="top" src={`${process.env.REACT_APP_API_URL}/api/userpost/images/${item._id}`} style={{ backgroundColor: imageLoaded ? "gray" : "red" }}
-                onLoad={handleImageLoad} className='userimg' />
+              <Card.Img
+                variant="top"
+                src={`${process.env.REACT_APP_API_URL}/api/userpost/images/${item._id}`}
+                style={{ backgroundColor: imageLoaded ? "gray" : "red", height: '25vw', border: '2px solid red', objectFit: 'contain' }}
+                onLoad={handleImageLoad}
+              />
 
-              <Card.Body>
-                {/* <Card.Title>Card Title</Card.Title> */}
 
+              <Card.Body style={{ padding: 0 }}>
+                {/* ... (rest of the Card.Body code remains unchanged) */}
                 <Imagebutton id={item._id} Contact={item.Contactnumber} />
-                {/* <Button variant="primary">Go somewhere</Button> */}
               </Card.Body>
             </Card>
           ))}
