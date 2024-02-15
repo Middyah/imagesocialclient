5537cb15d426f273768548f3ddd2548bf05a056b
Middyah <155668752+Middyah@users.noreply.github.com>
2024-02-16 00:25:19 +0530

    Add files via upload

---
+++ src/component/Bar.js
@@ -0,0 +1,80 @@
+import React, { useState,useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './css/SearchBar.css';
+
+const Bar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [category, setCategory] = useState('');
+  const navigate=useNavigate()
+console.log(category,"category");
+  const handleSearch = (e) => {
+    e.preventDefault();
+    onSearch(searchTerm, category);
+  };
+
+  const categories = [
+    //  'Product1',
+    //  'Service',
+    //  'Health',
+    //  'Education',
+    //  'Job',
+    //  'Lifestyle',
+    //  'Entertainment',
+    //  'Technology',
+    //  'Finance',
+    //  'Sports',
+    //  'Real Estate',
+    //  'Others',
+    //  'Website Activity',
+
+    { value: 0, label: 'All' },
+    { value: 0, label: "What's new" },
+    { value: 1, label: 'Product' },
+    { value: 2, label: 'Service' },
+    { value: 3, label: 'Health' },
+    { value: 4, label: 'Education' },
+    { value: 5, label: 'Job' },
+    { value: 6, label: 'Lifestyle' },
+    { value: 7, label: 'Entertainment' },
+    { value: 8, label: 'Technology' },
+    { value: 9, label: 'Finance' },
+    { value: 10, label: 'Sports' },
+    { value: 11, label: 'Gaming' },
+    { value: 12, label: 'Real Estate' },
+    { value: 13, label: 'Others' },
+    { value: 14, label: 'Website Activity' },
+  ];
+
+  const handleCategory=(e)=>{
+    setCategory(e.target.value)
+    localStorage.setItem("categorydata",e.target.value)
+    // navigate(`/mainarea?category=${e.target.value}`)
+    // localStorage.setItem("category",e.target.value)
+  }
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search for products..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <select
+          value={category}
+          onChange={(e) => handleCategory(e)}
+        >
+          <option value="">All Categories</option>
+          {categories.map((category,index) => (
+            <option key={category.value} value={category.value}>
+              {category.label}
+            </option>
+          ))}
+        </select>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
+};
+
+export default Bar;
---
+++ src/component/NavbarPost.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import './css/Header.css'; // Update the CSS file import
+import LocationDropdown from './LocationDropdown';
+import PopupPage from './PopupPage';
+import logo from './image/logo.png';
+import Bar from './Bar';
+import LocationDropdown2 from './LocationDropdown2';
+import {Route, Link, Routes, useLocation} from 'react-router-dom';
+const Header = () => {
+  const location = useLocation();
+  const pathname = location.pathname;
+console.log(pathname,"pathname");
+  return (
+    <>
+
+    <nav className={pathname==="/"?"":"header"}> 
+      <div className={pathname==="/"?"custom-navbar-content":"header-content"}> 
+        <div className={pathname==="/"?"custom-left-component":"left-component"}>  
+        {/* {pathname==="/"? <LocationDropdown />:<LocationDropdown2/>} */}
+        <LocationDropdown/>
+        </div>
+
+{pathname!=="/"&&(
+  <div className="center-component">
+          <img
+            className='logo_main'
+            src={logo}
+            alt="home"
+            style={{ marginBottom: '20px', marginTop: '50px' }}
+          />
+        </div>
+
+)}
+        
+        <div className={pathname==="/"?"custom-right-component":"right-component"}>
+          <PopupPage />
+        </div>
+
+        {pathname!=="/"&&(
+        <div className="additional-component">
+          <Bar />
+        </div>
+        )}
+
+      </div>
+    </nav>
+    </>
+   
+  );
+};
+
+export default Header;
