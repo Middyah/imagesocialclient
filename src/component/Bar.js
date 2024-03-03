1fc3e8668116a073d1b7113f21a2676bbac60ce8
Middyah <155668752+Middyah@users.noreply.github.com>
2024-03-03 12:04:46 +0530

    Update Bar.js

--- src/component/Bar.js
+++ src/component/Bar.js
@@ -1,39 +1,23 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './css/SearchBar.css';
 
 const Bar = ({ onSearch }) => {
-
-
   const searchParams = new URLSearchParams(document.location.search)
   let category1 = searchParams.get('category')
   const [searchTerm, setSearchTerm] = useState('');
   const [category, setCategory] = useState(category1);
-  const navigate=useNavigate()
-console.log(category,"category");
+  const navigate = useNavigate();
+
   const handleSearch = (e) => {
     // onSearch(searchTerm, category);
   };
-  useEffect(()=>{
+
+  useEffect(() => {
     onSearch(searchTerm);
-  },[searchTerm])
+  }, [searchTerm]);
 
-  console.log(searchTerm,"category1",category);
   const categories = [
-    //  'Product1',
-    //  'Service',
-    //  'Health',
-    //  'Education',
-    //  'Job',
-    //  'Lifestyle',
-    //  'Entertainment',
-    //  'Technology',
-    //  'Finance',
-    //  'Sports',
-    //  'Real Estate',
-    //  'Others',
-    //  'Website Activity',
-
     { value: 0, label: 'All' },
     { value: 15, label: "What's new" },
     { value: 1, label: 'Product' },
@@ -52,38 +36,43 @@ console.log(category,"category");
     { value: 14, label: 'Website Activity' },
   ];
 
-  const handleCategory=(e)=>{
+  const handleCategory = (e) => {
+    setCategory(e.target.value);
+    navigate(`/mainarea?category=${e.target.value}`);
+  };
 
-    setCategory(e.target.value)
-    // localStorage.setItem("categorydata",e.target.value)
-    navigate(`/mainarea?category=${e.target.value}`)
-    // localStorage.setItem("category",e.target.value)
-  }
-  useEffect(()=>{
-    localStorage.setItem("categorydata",category)
-  },[category])
+  useEffect(() => {
+    localStorage.setItem("categorydata", category);
+  }, [category]);
+
   return (
     <div className="search-bar">
-      
-        <input
-          type="text"
-          placeholder="Search for products..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <select
-          value={category}
-          onChange={(e) => handleCategory(e)}
-        >
-          <option value="">All Categories</option>
-          {categories.map((category,index) => (
-            <option key={category.value} value={category.value}>
-              {category.label}
-            </option>
-          ))}
-        </select>
-        <button onClick={handleSearch} type="submit">Search</button>
-      
+      <input
+        type="text"
+        placeholder="Search for products..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button
+        className="search-button"
+        onClick={handleSearch}
+        type="submit"
+        style={{ padding: '9px 20px' }} // Adjust padding to 9px
+      >
+        Search
+      </button>
+      <select
+        className="category-dropdown"
+        value={category}
+        onChange={(e) => handleCategory(e)}
+      >
+        <option value="">All Categories</option>
+        {categories.map((category, index) => (
+          <option key={category.value} value={category.value}>
+            {category.label}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
