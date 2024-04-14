d42de7386735eaf94f408a4ca0a56167891d3209
Middyah <155668752+Middyah@users.noreply.github.com>
2024-04-14 15:48:09 +0530

    Add files via upload

--- src/component/Imagebutton.js
+++ src/component/Imagebutton.js
@@ -72,6 +72,7 @@ export default function Imagebutton({ id, Contact }) {
       setShareCount(newShareCount);
       // Store updated share count in localStorage
       localStorage.setItem(`shareCount_${id}`, newShareCount);
+      await apiUrl.post(`/api/admin/post/share/${id}`);
     } catch (error) {
       console.error('Error sharing:', error);
     }
