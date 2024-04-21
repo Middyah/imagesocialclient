b565eb4af5970e8afb9c1a110d6b33a36e11163b
Middyah <155668752+Middyah@users.noreply.github.com>
2024-04-21 09:30:17 +0530

    Update Upload.js

--- src/component/Upload.js
+++ src/component/Upload.js
@@ -247,7 +247,7 @@ const Upload = () => {
                   style={{ marginLeft: '11px' }}
                   className='inputtext'
                 />
-                <select
+              {/*  <select
                   name="location"
                   id="location"
                   value={selectedLocation}
@@ -261,7 +261,7 @@ const Upload = () => {
                       {option}
                     </option>
                   ))}
-                </select>
+                </select>*/}
 
                 <select
                   name="category"
